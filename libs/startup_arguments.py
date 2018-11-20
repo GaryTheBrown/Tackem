6e4848f2d5e7a243f61ef8ed5832efa616a004a6
Gary The Brown <GaryBrownSunderland@gmail.com>
2018-11-20 17:09:47 +0000

    added windows appdir for cross platform usage

--- libs/startup_arguments.py
+++ libs/startup_arguments.py
@@ -1,16 +1,20 @@
 '''Startup Arguments Setup'''
 import argparse
 import os.path
-
+import sys
 PROGRAMNAME = "Tackem"
 PROGRAMVERSION = "ALPHA"
+PROGRAMCONFIGLOCATION = "~/." + PROGRAMNAME + "/"
+
+if sys.platform == "win32":
+    PROGRAMCONFIGLOCATION = os.environ['APPDATA'] + "/" + PROGRAMNAME + "/"
 
 def parse():
     '''parses the command line arguments'''
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--home',
                         help="Change the home folder location",
-                        default=os.path.expanduser("~/." + PROGRAMNAME + "/")
+                        default=os.path.expanduser(PROGRAMCONFIGLOCATION)
                        )
     parser.add_argument('--name',
                         help="Change the Programs Name",
