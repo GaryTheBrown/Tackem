543a66e7a2f42e6c146ff4d4fd488149ba3facf5
Gary The Brown <GaryBrownSunderland@gmail.com>
2019-02-02 01:38:48 +0000

    fix an indentation

--- plugins/ripping/ripper/www/__init__.py
+++ plugins/ripping/ripper/www/__init__.py
@@ -16,7 +16,7 @@ def mounts(key, systems, plugins, config):
     root.labeler = Labeler("Ripper Labeler", key, systems, plugins, config,
                            base_stylesheet=stylesheet)
     root.converter = Converter("Ripper Converter", key, systems, plugins, config,
-                           base_stylesheet=stylesheet)
+                               base_stylesheet=stylesheet)
     cherrypy.tree.mount(root,
                         config.get("webui", {}).get("baseurl", "/") + key.replace(" ", "/") + "/",
                         cfg(config))
