47facc51c4b8110d22a7bc0725fec0e84a497299
Gary The Brown <GaryBrownSunderland@gmail.com>
2019-02-21 21:51:33 +0000

    changed select to allow a grab data without any conditions

--- libs/sql/baseclass.py
+++ libs/sql/baseclass.py
@@ -79,18 +79,21 @@ class MysqlBaseClass(metaclass=ABCMeta):
         command += ", ".join(values) + ");"
         return self._call(SQLMessage(system_name, command=command))
 
-    def select(self, system_name, table_name, dict_of_values, list_of_returns=None):
+    def select(self, system_name, table_name, dict_of_values=None, list_of_returns=None):
         '''select data from a table'''
         returns = "*"
         if isinstance(list_of_returns, list):
             returns = ", ".join(list_of_returns)
         elif isinstance(list_of_returns, str):
             returns = list_of_returns
-        command = "SELECT " + returns + " FROM " + table_name + " WHERE "
-        values = []
-        for key in dict_of_values:
-            values.append(key + " = " + self._convert_var(dict_of_values[key]))
-        command += " AND ".join(values) + ";"
+        command = "SELECT " + returns + " FROM " + table_name
+        if dict_of_values:
+            command += " WHERE "
+            values = []
+            for key in dict_of_values:
+                values.append(key + " = " + self._convert_var(dict_of_values[key]))
+            command += " AND ".join(values)
+        command += ";"
         return self._call(SQLMessage(system_name, command=command, return_data=[]))
 
 
