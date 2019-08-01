e593cd3e3d1b0dc5da8bf5829914766671820b65
Gary The Brown <GaryBrownSunderland@gmail.com>
2019-08-01 03:29:44 +0100

    removed plugins that have been moved to there own repos

--- plugins/library/games/api/__init__.py
+++
@@ -1,5 +0,0 @@
-'''API FOR PLUGIN'''
-def api(kwargs, apimode):
-    '''Games Library api'''
-
-    return "GAMES API"
--- plugins/library/movies/api/__init__.py
+++
@@ -1,5 +0,0 @@
-'''API FOR PLUGIN'''
-def api(kwargs, apimode):
-    '''Movie Library api'''
-
-    return "MOVIE API"
