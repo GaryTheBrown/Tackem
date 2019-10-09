84b54db6ffb167ce61904dbb08362c436322e423
Gary The Brown <GaryBrownSunderland@gmail.com>
2019-10-09 21:56:42 +0000

    improved TackemSyatemPlugin

--- system/plugin.py
+++ system/plugin.py
@@ -7,40 +7,67 @@ class TackemSystemPlugin(TackemSystemBase):
 
 
     def __init__(self, plugin_type, plugin_name, instance=None):
-        self._plugin_type = plugin_type
-        self._plugin_name = plugin_name
-        self._name = plugin_type + " " + plugin_name
-        self._instance = instance
-        if isinstance(self._instance, str):
-            self._name += " " + instance
+        self.__plugin_type = plugin_type
+        self.__plugin_name = plugin_name
+        self.__plugin_full_name = plugin_type + " " + plugin_name
+        self.__plugin_instance = instance
+        if isinstance(self.__plugin_instance, str):
+            self.__plugin_full_name += " " + instance
 
-        self._p_config = None
-        self._p_plugin = None
-        # self._p_system = None
-        _, temp_config = self.get_config(['plugins', self._plugin_type, self._plugin_name], {})
+        self.__p_config = None
+        self.__p_plugin = None
+        self.__p_system = None
+
+        _, temp_config = self.get_config(['plugins', self.__plugin_type, self.__plugin_name], {})
         with self._base_data.config_lock:
-            if self._instance:
-                self._p_config = temp_config[self._instance]
+            if self.__plugin_instance:
+                self.__p_config = temp_config[self.__plugin_instance]
             else:
-                self._p_config = temp_config
+                self.__p_config = temp_config
 
         with self._base_data.plugins_lock:
-            self._p_plugin = self._base_data.plugins[self._plugin_type][self._plugin_name]
+            self.__p_plugin = self._base_data.plugins[self.__plugin_type][self.__plugin_name]
 
-        # with self._base_data.systems_lock:
-        #     self._p_system = self._base_data.systems[self._name]
+        with self._base_data.systems_lock:
+            self.__p_system = self._base_data.systems[self.__plugin_full_name]
 
 
+    @property
     def config(self):
         '''return plugins config'''
-        return self._p_config
+        return self.__p_config
 
 
+    @property
     def plugin(self):
         '''return plugin'''
-        return self._p_plugin
+        return self.__p_plugin
 
 
-    # def system(self):
-    #     '''return plugins system'''
-    #     return self._p_system
+    @property
+    def system(self):
+        '''return plugins system'''
+        return self.__p_system
+
+
+    @property
+    def plugin_type(self):
+        '''returns plugin type'''
+        return self.__plugin_type
+
+
+    @property
+    def plugin_name(self):
+        '''returns plugin name'''
+        return self.__plugin_name
+
+
+    @property
+    def plugin_instance(self):
+        '''returns plugin instance name'''
+        return self.__plugin_instance
+
+    @property
+    def plugin_full_name(self):
+        '''returns plugin instance name'''
+        return self.__plugin_full_name
