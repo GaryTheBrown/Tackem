32025af80852e082ac18a57fdef8a5f434787094
Gary The Brown <GaryBrownSunderland@gmail.com>
2021-02-27 00:57:18 +0000

    another small fix of the trheadding exit of ISO

--- libs/watcher.py
+++ libs/watcher.py
@@ -24,40 +24,34 @@ class FolderWatcher:
 
         self._thread = threading.Thread(target=self.run, args=())
         self._thread.setName(f"Folder Watcher: {folder}")
+        self.__thread_run = True
         self._thread.start()
-        # self.thread_run = True
-        print(f"Started Folder Watcher: {folder}")
-
-    @property
-    def thread_run(self) -> bool:
-        '''return if thread is running'''
-        return self._thread.is_alive()
 
     def stop_thread(self):
         '''stop the thread'''
         if self._thread.is_alive():
-            self.thread_run = False
+            self.__thread_run = False
             self._thread.join()
-#TODO Is this really working???
+
     def run(self):
         '''Folder Watcher Script'''
-        while self.thread_run:
+        while self.__thread_run:
             if self.__recursive:
                 new_count = len(os.listdir(self.__folder))
             else:
                 new_count = len(next(os.walk(self.__folder))[2])
-            if not self.thread_run:
+            if not self.__thread_run:
                 return
             if isinstance(self.__folder_count, int):
                 if self.__folder_count != new_count:
                     self.__callable()
             else:
                 self.__folder_count = new_count
-            if not self.thread_run:
+            if not self.__thread_run:
                 return
             sleep_timer = 0
             while sleep_timer < self.__sleep_timer:
                 time.sleep(self.__single_sleep)
-                if not self.thread_run:
+                if not self.__thread_run:
                     return
                 sleep_timer += self.__single_sleep
